a308716a7bf598ad6f5076e3ab1641c580453ec6
Ri0n72Y <tzy673570751@gmail.com>
2025-07-25 14:34:39 +0800

    feat: example and data

--- helloWorld.py
+++ helloWorld.py
@@ -2,7 +2,19 @@
 
 # 定义一个函数，让调用更加结构化
 def hello():
-    print("Hello, World!")
+    print("***** Hello, World! *****")
+
+
+def age_greater_than(k: int):
+    l = [
+        {"name": "Alice", "age": 25},
+        {"name": "Bob", "age": 30}
+    ]
+    for item in l:
+        if item["age"] > k:
+            return item["name"], item["age"]
+        elif item["age"] < 28:
+            print(f"{item['name']} is younger than 28")
 
 
 # https://www.runoob.com/python3/python3-name-main.html
@@ -11,6 +23,8 @@ def hello():
 # 这使得模块可以被重用而不会执行不必要的代码
 if __name__ == "__main__":
     hello()
+    name, age = age_greater_than(28)
+    print("result: ", name, age)
 
 # 而这部分会在脚本被导入时执行
 hello()
